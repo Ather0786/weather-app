a2e618e5c3e42af66097e3649a84012d02698fa0
unknown <kaifather403@gmail.com>
2026-09-02 16:46:04 +0530

    Fix Vercel deployment

--- server.js
+++ server.js
@@ -111,6 +111,4 @@ app.get("/profile", authenticateToken, async (req, res) => {
         });
     }
 });
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+module.exports = app;
